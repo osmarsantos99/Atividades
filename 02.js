b918e128bc2f059bb1e5f6e1d1d3b8bf419c3095
osmarsantos99 <osmarsantos99@hotmail.com>
2021-10-14 13:35:56 -0300

    Melhorando formatação

--- 02.js
+++ 02.js
@@ -1,6 +1,6 @@
 // Calculo das operações matemáticas
-const number1=12
-const number2=8
+const number1 = 12
+const number2 = 8
 console.log (number1 + number2)
 console.log (number1 - number2)
 console.log (number1 / number2)
